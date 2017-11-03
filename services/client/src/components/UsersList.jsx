b0ac3cc4d649eb8e368b45006897ed89f43dc70c
Michael Herman <hermanmu@gmail.com>
2017-11-02 21:04:40 -0600

    updated UsersList component (part 3)

--- services/client/src/components/UsersList.jsx
+++ services/client/src/components/UsersList.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 
 const UsersList = (props) => {
   return (
     <div>
       <h1>All Users</h1>
       <hr/><br/>
-      {
-        props.users.map((user) => {
-          return (
-            <h4
-              key={user.id}
-              className="well"
-            >{user.username}
-            </h4>
-          )
-        })
-      }
+      <Table striped bordered condensed hover>
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>Email</th>
+            <th>Username</th>
+            <th>Active</th>
+            <th>Admin</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            props.users.map((user) => {
+              return (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.email}</td>
+                  <td>{user.username}</td>
+                  <td>{String(user.active)}</td>
+                  <td>{String(user.admin)}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </Table>
     </div>
   )
 };
