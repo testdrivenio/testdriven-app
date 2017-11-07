bb9a541885ab4fc9283f5800b4d7bea5893b5e33
Michael Herman <hermanmu@gmail.com>
2017-11-07 00:54:09 -0700

    added test script and testcafe (part 4)

---
+++ test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+env=$1
+file=""
+fails=""
+
+if [[ "${env}" == "stage" ]]; then
+  file="docker-compose-dev.yml"
+elif [[ "${env}" == "dev" ]]; then
+  file="docker-compose-dev.yml"
+elif [[ "${env}" == "prod" ]]; then
+  file="docker-compose-prod.yml"
+else
+  echo "USAGE: sh test.sh environment_name"
+  echo "* environment_name: must either be 'dev', 'stage', or 'prod'"
+  exit 1
+fi
+
+inspect() {
+  if [ $1 -ne 0 ]; then
+    fails="${fails} $2"
+  fi
+}
+
+docker-compose -f $file run users-service python manage.py test
+inspect $? users
+docker-compose -f $file run users-service flake8 project
+inspect $? users-lint
+testcafe chrome e2e
+inspect $? e2e
+if [[ "${env}" != "stage" ]]; then
+  docker-compose -f $file run client npm test -- --coverage
+  inspect $? client
+fi
+
+if [ -n "${fails}" ]; then
+  echo "Tests failed: ${fails}"
+  exit 1
+else
+  echo "Tests passed!"
+  exit 0
+fi
