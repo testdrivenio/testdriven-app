f7f4c12bac37149de59a46fa0e7bc818df12bfdc
Michael Herman <hermanmu@gmail.com>
2017-11-27 19:40:55 -0700

    react refactor (part 6)

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -9,6 +9,7 @@ import Form from './components/forms/Form';
 import Logout from './components/Logout';
 import UserStatus from './components/UserStatus';
 import Message from './components/Message';
+import Footer from './components/Footer';
 
 class App extends Component {
   constructor() {
@@ -83,6 +84,9 @@ class App extends Component {
               <br/>
               <Switch>
                 <Route exact path='/' render={() => (
+                  <p>Something.</p>
+                )} />
+                <Route exact path='/all-users' render={() => (
                   <UsersList
                     users={this.state.users}
                   />
@@ -119,6 +123,7 @@ class App extends Component {
             </div>
           </div>
         </div>
+        <Footer/>
       </div>
     )
   };
---
+++ services/client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = (props) => (
+  <footer className="footer">
+    <div className="container">
+      <small className="text-muted">
+        <span>Copyright 2017 <a href="http://testdriven.io">TestDriven.io</a>.</span>
+      </small>
+    </div>
+  </footer>
+)
+
+export default Footer;
