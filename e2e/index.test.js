565275e03f0814c1ad5452423cbae8b498869a7b
Michael Herman <hermanmu@gmail.com>
2017-11-29 18:09:43 -0700

    Added lambda (part 6)

--- e2e/index.test.js
+++ e2e/index.test.js
@@ -1,56 +1,18 @@
 import { Selector } from 'testcafe';
 
-const randomstring = require('randomstring');
-
-const username = randomstring.generate();
-const email = `${username}@test.com`;
-const password = 'greaterthanten';
-
 const TEST_URL = process.env.TEST_URL;
 
 
 fixture('/').page(`${TEST_URL}/`);
 
-test(`should display the page correctly if a user is not logged in`, async (t) => {
+test(`should display the all-users page correctly if a user is not logged in`, async (t) => {
   await t
     .navigateTo(TEST_URL)
-    .expect(Selector('.ace_content').innerText).contains(
-      '# Enter your code here.')
-    .typeText('.ace_editor', 'Expected text')
-    .expect(Selector('.ace_content').innerText).contains('Expected text')
-    .expect(Selector('h1').withText('Exercises').exists).ok()
     .expect(Selector('a').withText('User Status').exists).notOk()
     .expect(Selector('a').withText('Log Out').exists).notOk()
     .expect(Selector('a').withText('Register').exists).ok()
     .expect(Selector('a').withText('Log In').exists).ok()
     .expect(Selector('a').withText('Swagger').exists).ok()
     .expect(Selector('a').withText('Users').exists).ok()
-    .expect(Selector('button').withText('Run Code').exists).notOk()
-    .expect(Selector('.alert-warning').withText(
-      'Please log in to submit an exercise.').exists).ok()
-});
-
-test(`should display the page correctly if a user is logged in`, async (t) => {
-  await t
-    .navigateTo(`${TEST_URL}/register`)
-    .typeText('input[name="username"]', username)
-    .typeText('input[name="email"]', email)
-    .typeText('input[name="password"]', password)
-    .click(Selector('input[type="submit"]'))
-    .navigateTo(TEST_URL)
-    .expect(Selector('.ace_content').innerText).contains(
-      '# Enter your code here.')
-    .typeText('.ace_editor', 'Expected text')
-    .expect(Selector('.ace_content').innerText).contains('Expected text')
-    .expect(Selector('h1').withText('Exercises').exists).ok()
-    .expect(Selector('a').withText('User Status').exists).ok()
-    .expect(Selector('a').withText('Log Out').exists).ok()
-    .expect(Selector('a').withText('Register').exists).notOk()
-    .expect(Selector('a').withText('Log In').exists).notOk()
-    .expect(Selector('a').withText('Swagger').exists).ok()
-    .expect(Selector('a').withText('Users').exists).ok()
     .expect(Selector('.alert').exists).notOk()
-    .expect(Selector('button').withText('Run Code').exists).ok()
-    .expect(Selector('.alert-warning').withText(
-      'Please log in to submit an exercise.').exists).notOk()
 });
