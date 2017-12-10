9e54735a4b2c71301475aa68d24ea49421f57f26
Michael Herman <hermanmu@gmail.com>
2017-12-10 10:20:22 -0700

    added scores service (part 7)

---
+++ init_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# create
+docker-compose -f docker-compose-dev.yml run exercises python manage.py recreate_db
+docker-compose -f docker-compose-dev.yml run users-service python manage.py recreate_db
+docker-compose -f docker-compose-dev.yml run scores python manage.py recreate_db
+# seed
+docker-compose -f docker-compose-dev.yml run exercises python manage.py seed_db
+docker-compose -f docker-compose-dev.yml run users-service python manage.py seed_db
---
+++ services/scores/manage.py
@@ -0,0 +1,65 @@
+# services/scores/manage.py
+
+
+import unittest
+import coverage
+
+from flask_script import Manager
+from flask_migrate import MigrateCommand
+
+from project import create_app, db
+
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+        'project/__init__.py'
+    ]
+)
+COV.start()
+
+
+app = create_app()
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    manager.run()
